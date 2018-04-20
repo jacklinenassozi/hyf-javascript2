841d39b3c944b008ba458815acea33e20830df6f
jacklinenassozi <36820753+jacklinenassozi@users.noreply.github.com>
2018-04-20 23:16:25 +0200

    Add files via upload

---
+++  week4/main.js
@@ -0,0 +1,67 @@
+/**
+ * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
+ * @param {integer} min - The min number
+ * @param {integer} max - The max number
+ * @returns {Number} Random number between min and max
+ */
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+/**
+* Get an array with car objects with random color and speed
+* @param {integer} numberOfCars - The number of cars 
+* @returns {array} Array containing the car objects
+*/
+function generateCars(numberOfCars) {
+  const cars = [];
+
+  const carMakes = ['Honda', 'BMW','Fiat','Skoda','Volvo'];
+  const carColors = ['lightgrey', 'lightcyan','lightyellow','lightgreen','lightcoral','lightpink'];
+  
+  for (let i = 0; i < numberOfCars; i++) {
+      const car = {};
+      const randomMakeIndex = randomIntFromInterval(0, carMakes.length - 1);
+      const randomColorIndex = randomIntFromInterval(0, carColors.length - 1);
+
+      car.make = carMakes[randomMakeIndex];
+      car.color = carColors[randomColorIndex];
+      car.speed = randomIntFromInterval(0, 100);
+
+      cars.push(car);
+  }
+
+  return cars;
+}
+
+  
+  const cars = generateCars(10);
+  
+  console.log(cars);
+  
+  const filteredCars = cars.filter(function (car) {
+    return (car.speed >= 30 && car.speed <= 60);
+  });
+  
+  console.log(filteredCars);
+  
+  const filteredCars2 = cars.filter(function (car) {
+    return (car.color !== 'lightyellow');
+  });
+  
+  const filteredCars2JustMakes = filteredCars2.map(function (car) {
+    return car.make;
+  });
+  
+  console.log(filteredCars2JustMakes);
+  
+  const danishCars = cars.map(function (car) {
+    return {
+      maerke: car.make,
+      fart: car.speed,
+      farve: car.color,
+    }
+  });
+  console.log(danishCars);
+  
